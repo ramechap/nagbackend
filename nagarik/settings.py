9810edba7c59e0833d9a119e4d98e23c7b6db5f7
ramechap <chaprame2@gmail.com>
2025-04-23 16:26:49 +0545

    ic update

--- nagarik/settings.py
+++ nagarik/settings.py
@@ -60,6 +60,7 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     "whitenoise.middleware.WhiteNoiseMiddleware",
     
+    
 ]
 CORS_ORIGIN_ALLOW_ALL = True
 ROOT_URLCONF = 'nagarik.urls'
